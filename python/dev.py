77a8697450a98e3cf4cdee732533cdf8a6317c3d
arkenthera <arkengraphics@gmail.com>
2016-01-27 23:13:36 +0200

    Adding magic..

--- python/dev.py
+++ python/dev.py
@@ -3,17 +3,7 @@ import os
 import argparse
 from requests.auth import HTTPBasicAuth
 import requests
+import time
+t = time.strftime("%I:%M:%S")
 
-parser = argparse.ArgumentParser(description='Pull and push for all Chiika repositories');
-parser.add_argument('--pull', help='pull',action='store_true');
-parser.add_argument('--push', help='push',action='store_true');
-
-args = parser.parse_args();
-pull = args.pull;
-push = args.push;
-
-git_command_add_all                 = ['git','add','--all']
-git_command_commit                  = ['git','commit','--all','-m','Commit-Bot']
-
-r = requests.get('https://api.github.com/user', auth=HTTPBasicAuth('arkenthera', 'sezalpg44242'))
-print r.status_code
+print t
