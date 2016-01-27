0367f164a5f63054476b2dab5223e1703a0f810a
arkenthera <arkengraphics@gmail.com>
2016-01-27 23:19:42 +0200

    "Weeabot - Auto commit 27/01/2016 11:19:42""

--- python/dev.py
+++ python/dev.py
@@ -4,6 +4,30 @@ import argparse
 from requests.auth import HTTPBasicAuth
 import requests
 import time
-t = time.strftime("%I:%M:%S")
+t = time.strftime("%d/%m/%Y %I:%M:%S")
 
-print t
+parser = argparse.ArgumentParser(description='Pull and push for all Chiika repositories');
+parser.add_argument('--pull', help='pull',action='store_true');
+parser.add_argument('--push', help='push',action='store_true');
+
+args = parser.parse_args();
+pull = args.pull;
+push = args.push;
+
+git_command_add_all                 = ['git','add','--all']
+git_command_commit                  = ['git','commit','--all','-m','"Weeabot - Auto commit ' + t + '""']
+git_command_push                    = ['git','push']
+
+chiika  = os.getcwd() + "/../";
+chiikaNode  = os.getcwd() + "/../../chiika-node";
+chiikaApi   = os.getcwd() + "/../../ChiikaApi";
+
+#Pushing Chiika
+git_query = Popen(git_command_add_all, cwd=chiika, stdout=PIPE, stderr=PIPE)
+(git_status, error) = git_query.communicate()
+
+git_query = Popen(git_command_commit, cwd=chiika, stdout=PIPE, stderr=PIPE)
+(git_status, error) = git_query.communicate()
+
+git_query = Popen(git_command_push, cwd=chiika, stdout=PIPE, stderr=PIPE)
+(git_status, error) = git_query.communicate()
